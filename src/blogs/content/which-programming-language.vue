48317a5d789a8411c7cc1193c52209e9687b5e23
Lanthier <lanthier72@gmail.com>
2020-09-01 12:49:14 -0500

    which programming language blog

---
+++ src/blogs/content/which-programming-language.vue
@@ -0,0 +1,301 @@
+<template>
+  <section>
+    <p>
+      Let me skip the anticipation. It does not <em>really</em> matter which programming language you learn first. At least, that is what you will hear
+      from most forum posters and other bloggers in this space. But what if you had to choose? Maybe you only want to learn one programming language,
+      or perhaps you are switching careers and need to know which programming language will get you on the fast track. Either way, I will do an in-depth
+      analysis on some programming languages.
+    </p>
+    <h1 class="subtitle">
+      Considerations for your first programming language
+    </h1>
+    <strong>
+      Industry application
+    </strong>
+    <p>
+      The biggest factor: Why do you want to learn how to program? Do you want to make video games or websites? Certain programming languages are better for particular
+      career paths. Python is ideal for data science, for instance.
+    </p>
+    <strong>
+      Language type
+    </strong>
+    <p>
+      I believe your first language should be a language that best fits your learning style. When you start coding, would you prefer to see what is happening
+      behind the scenes in your code? Or would you rather the code magically work?
+    </p>
+    <strong>
+      Language market
+    </strong>
+    <p>
+      Do you want to learn a language that is popular or a niche language? Niche languages can make you more money but frequently have smaller communities
+      to help you out when you get stuck. I recommend my students to first learn a popular language so they have a lot of support.
+    </p>
+    <strong>
+      Language difficulty
+    </strong>
+    <p>
+      The time commitment you will need to give will vary based on how difficult your language is to learn.
+    </p>
+    <h1 class="subtitle">
+      Industry application
+    </h1>
+    <p>
+      So, what do you want to do?
+    </p>
+    <strong>
+      Video Games
+    </strong>
+    <p>
+      I recommend <strong>C#</strong>. Everyone will tell you <strong>C++</strong> is the first language you should learn for video games, but C++
+      is tough as a first programming language. It is harder to master and there is a lot more work involved to program simplistic operations. C# is a
+      modern framework and is the primary language of <a href="https://unity.com/">Unity</a> framework. Following my recommendation does not mean you
+      can't also learn C++, but you might have better luck learning C# first.
+    </p>
+    <p>
+      C++ is typically the language people recommend because we popularly know the video game industry for using it to improve the speed of code.
+      C++ can work "closer" to the computer to better manage the memory the program uses. Thus, making the code faster. I don't think this is a good reason for
+      someone to learn C++ as their first language.
+    </p>
+    <!-- <p>
+      Programming language to learn for video games:
+    </p> -->
+    <table class="table">
+      <thead>
+        <tr>
+          <th>
+            Programming Language
+          </th>
+          <th>
+            Category
+          </th>
+          <th>
+            Top Game Engine(s)
+          </th>
+          <th>
+            Games*
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            C++
+          </td>
+          <td>
+            AA/AAA
+          </td>
+          <td>
+            <a href="https://www.unrealengine.com">Unreal Engine</a>
+          </td>
+          <td>
+            World of Warcraft, Fortnite, Borderlands
+          </td>
+        </tr>
+        <tr>
+          <td>
+            C#
+          </td>
+          <td>
+            AA/AAA
+          </td>
+          <td>
+            <a href="https://unity.com/">Unity</a>
+          </td>
+          <td>
+            Pokemon Go, Hearthstone, Crossy Road
+          </td>
+        </tr>
+        <tr>
+          <td>
+            JavaScript
+          </td>
+          <td>
+            Web
+          </td>
+          <td>
+            <a href="https://www.babylonjs.com/">babaylonjs</a>, <a href="https://phaser.io/">phaser</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Lua
+          </td>
+          <td>
+            AA/AAA
+          </td>
+          <td>
+            <a href="https://love2d.org/">Love</a>
+          </td>
+          <td>
+            Dota 2, Dark Souls, Roblox
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Swift
+          </td>
+          <td>
+            IOS Mobile
+          </td>
+          <td>
+            <a href="https://developer.apple.com/scenekit/">SceneKit</a>
+          </td>
+          <td>
+            Angry Birds
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Objective-C
+          </td>
+          <td>
+            IOS Mobile
+          </td>
+          <td>
+            <a href="https://gamua.com/sparrow/">Sparrow</a>, <a href="https://www.cocos.com/en/">Cocos 2D</a>
+          </td>
+          <td>
+            Star Trek: Rivals, WordScape
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Java
+          </td>
+          <td>
+            AA/AAA, Android Mobile
+          </td>
+          <td>
+            <a href="https://libgdx.badlogicgames.com/">libGDX</a>, <a href="https://jmonkeyengine.org/">jmonkeyengine</a>
+          </td>
+          <td>
+            The Amazing Spiderman 2, Assassin's Creed II
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <em>* Many games are created using multiple languages.</em>
+    <br /><br />
+    <strong>
+      Website Development
+    </strong>
+    <p>
+      <b>JavaScript</b> is a quick language to use, albeit a little difficult to master. JavaScript is useful regardless if you want to be a front-end
+      (what you see in your browser), back-end (the data and authentication) or a full-stack (both) developer. If your goal is to become a full-stack developer,
+      you will need to follow both the front-end and the back-end paths of learning.
+    </p>
+    <p>
+      I recommend JavaScript because, while you can code front ends in Python, JavaScript is still by far the most dominate programming language in web development.
+      Browsers also directly support JavaScript, which makes it easy to work with it right out of the box.
+    </p>
+    <p>
+      <b>Front-end developers</b> need to know HTML, CSS and JavaScript at a minimum. HTML and CSS are what you see on your screen, and the JavaScript
+      manipulates those things on your screen to create functional behavior. HTML is the button, CSS is what the button looks like and JavaScript
+      is what happens when you click that button.
+    </p>
+    <p>
+      <b>Back-end developers</b> need to know a back-end language and a database query language. The back-end typically involves the storage of data that might
+      be used across multiple websites (or even mobile apps!). Back-end also involves creating interfaces for these websites to use the data. I recommend JavaScript
+      as your first back-end language. Other back-end languages include C#, PHP, Java and Python.
+    </p>
+    <strong>
+      Mobile Application Development
+    </strong>
+    <p>
+      <b>Swift</b> for IOS or <b>Java</b> for Android. Keep in mind that if you learn Swift, there are very few popular applications outside of IOS development. Java
+      on the other hand is a very portable language and still widely used across different industries.
+    </p>
+    <p>
+      You can also learn JavaScript or C# to create cross-platform targeting applications. C# uses the <a href="https://dotnet.microsoft.com/apps/xamarin">Xamarin</a>
+      framework and JavaScript uses <a href="https://reactnative.dev/">React Native</a>. I recommend staying away from Xamarin both from personal experience and
+      from a <a href="https://insights.stackoverflow.com/survey/2020#technology-most-loved-dreaded-and-wanted-other-frameworks-libraries-and-tools-dreaded3">February 2020 Stack Overflow survey</a>.
+      I took a snapshot of the survey results below for "Most Dreaded Technology".
+    </p>
+    <img src="../../assets/stack-overflow-dreaded.png" />
+    <h1 class="subtitle">
+      Language Type and Classifications
+    </h1>
+    <p>
+      If you are still stuck between a couple languages or you don't have a clear goal in mind, language type may be the deciding factor for you.
+    </p>
+    <b>
+      Type Class
+    </b>
+    <p>
+      <b>Strongly typed</b> or <b>weakly typed</b>. Roughly speaking, strongly typed needs to specify if something is a number or a word. Weakly typed languages do not need you
+      to specify. Mostly, this is true when you are converting types into other types. I.E. Trying to turn a number into a word in your code. Consider the following:
+    </p>
+    <prism :code="codeSnippet1" language="python" />
+    <p>
+      Weakly typed languages such as Python allow for more freedom with your code. What if you don't know whether your variable will hold a number or a string? The downside to
+      weakly typed languages is you are more prone to error. For a first-time programmer, it isn't obvious at all that there is any kind of typing going on here.
+    </p>
+    <prism :code="codeSnippet2" language="java" />
+    <p>
+      Strongly typed languages like Java and C# force you to declare which type the variable is. Java is a popular language for new programmers because it is very
+      verbose in the code you write: <pre class="inline">Scanner scanner = new Scanner();</pre>. The downsides are you aren't allowed as much
+      freedom and you have to write more code.
+    </p>
+    <b>
+      Compilation Class
+    </b>
+    <p>
+      <b>Static</b> or <b>dynamic</b>. Static typing means the program checks your types before running your program. Dynamic typing means the program checks
+      your types while your program is running. I find that when I teach new programmers, they have a little more trouble with dynamically typed languages, but this isn't
+      always the case. Dynamically typed languages give you some freedom too, though this freedom that might not matter for your first programming language.
+    </p>
+    <h1 class="subtitle">
+      Language Difficulty
+    </h1>
+    <p>
+      How difficult a language is maybe subjective, but there are some indicators and language "features" that can help us distinguish which programming
+      languages are harder to learn than others. Once again I'm going to use a snapshot from the 2020 developer survey from stack overflow:
+    </p>
+    <img src="../../assets/stack-overflow-dreaded-language.png" />
+    <p>
+      VBA is an older language which was developed by Microsoft. Programmers write it for products and frameworks in Mircosoft's ecosystem. C# has largely replaced VBA. Objective-C
+      is the primary language you use to write software for IOS and OS X, though Swift is starting to replace some of this. I would not recommend VBA, Objective-C,
+      Perl, Assembly, C no PHP as your first language.
+    </p>
+    <h1 class="subtitle">
+      Language Market and Popularity
+    </h1>
+    <p>
+      Uncommon and older programming languages seem to offer the highest pay, according to the 2020 Stack Overflow Developer Survey. Personally, I couldn't handle working
+      with Scala or Objective-C all day, but that's just me.
+    </p>
+    <img src="../../assets/stack-overflow-pay-by-language.png" />
+    <h1 class="subtitle">
+      So, Which Programming Language?
+    </h1>
+    <p>
+      If you still don't know, just learn JavaScript. JavaScript has applications in more fields than just web development. It is simple to start up and forgives you
+      where other languages would not. It will expose to functional programming and give you all the basics of languages like variables, functions, types, etc. Also,
+      recent developments in JavaScript allow for more object-oriented programming.
+    </p>
+  </section>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class WhichProgrammingLanguage extends Vue {
+  get codeSnippet1 () {
+    return `# Python is a weakly typed language. You can change strings (characters/words) to numbers and multiply them.
+
+aWord = 'Hello'
+x = aWord*2 # No error
+aWord = 123 # No error, possible to turn strings into numbers`
+  }
+
+  get codeSnippet2 () {
+    return `// Java is a strongly typed language; you cannot change types of variables.
+
+string aWord = "Hello";
+int x = aWord * 2; // Error can't multiply string by integer
+aWord = 123; // Also error`
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
